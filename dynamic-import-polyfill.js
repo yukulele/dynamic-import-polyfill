4a95361d4381f019e9c5d4175d2516eb624cd5db
Clément P <yukulele@users.noreply.github.com>
2017-09-06 13:47:49 +0200

    Create dynamic-import-polyfill.js

---
+++ dynamic-import-polyfill.js
@@ -0,0 +1,19 @@
+{
+  const map = new Map()
+  window._import = url => {
+    if (map.has(url)) {
+      return map.get(url)
+    }
+    const promise = fetch(url)
+    .then(reponse => reponse.text())
+    .then(text => {
+      const fct = new Function('module', text)
+      const module = {}
+      map.set()
+      fct(module)
+      return module.exports
+    })
+    map.set(url, promise)
+    return promise
+  }
+}
