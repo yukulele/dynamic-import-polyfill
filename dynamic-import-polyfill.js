3f7f4cf8e6c541b65bfdbd2709528e9b4c5e7110
Clément P <yukulele@users.noreply.github.com>
2017-09-06 14:01:33 +0200

    Update dynamic-import-polyfill.js
    * add default `module.exports` attribute
    * prettier

--- dynamic-import-polyfill.js
+++ dynamic-import-polyfill.js
@@ -5,14 +5,14 @@
       return map.get(url)
     }
     const promise = fetch(url)
-    .then(reponse => reponse.text())
-    .then(text => {
-      const fct = new Function('module', text)
-      const module = {}
-      map.set()
-      fct(module)
-      return module.exports
-    })
+      .then(reponse => reponse.text())
+      .then(text => {
+        const fct = new Function('module', text)
+        const module = { exports: {} }
+        map.set()
+        fct(module)
+        return module.exports
+      })
     map.set(url, promise)
     return promise
   }
